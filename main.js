63ac12326b77206cbf6fa53eb376bb8cefd37712
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-01 21:03:36 -0300

    add new file

---
+++ main.js
@@ -0,0 +1,6 @@
+const moedas_convert = document.querySelector('#moedas_convert');
+const moedas_result = document.querySelector('#moedas_result');
+const montante_convert = document.querySelector('#montante_convert');
+const montante_result = document.querySelector('#montante_result');
+const trocar = document.querySelector('#trocar');
+const valor = document.querySelector('#valor');
