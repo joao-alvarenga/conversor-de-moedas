69bdc45d60d2c96094dbc6c30ffb5df390b5fbbc
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-01 21:14:04 -0300

    add updates

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ trocar.addEventListener('click', ()=>{
     moedas_result.value= temp;
     compute();
 });
+
+function compute(){
+    const moedas_convert1 = moedas_convert.value;
+    const moedas_result1 = moedas_result.value;
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${moedas_convert1}`)
+    .then(res => res.json())
+    .then(res => {
+        const new_rate = res.rates[moedas_convert1];
+        valor.innerText = `1 ${moedas_convert1} = ${new_rate} ${moedas_result1}`
+        montante_result.value = (montante_convert.value * new_rate).toFixed(2);
+    })
+}
+
+compute();
