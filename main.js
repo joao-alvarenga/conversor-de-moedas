c8f1b7dc00e2290c6d0e7fd1a65ee203c4b78512
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-01 21:07:15 -0300

    add updates

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const montante_convert = document.querySelector('#montante_convert');
 const montante_result = document.querySelector('#montante_result');
 const trocar = document.querySelector('#trocar');
 const valor = document.querySelector('#valor');
+
+moedas_convert.addEventListener('change', compute);
+moedas_result.addEventListener('change', compute);
+montante_convert.addEventListener('input', compute);
+montante_result.addEventListener('input', compute);
+
+trocar.addEventListener('click', ()=>{
+    const temp = moedas_convert.value;
+    moedas_convert.value = moedas_result.value;
+    moedas_result.value= temp;
+    compute();
+});
